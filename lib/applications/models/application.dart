24f53c554f5f048b0b382e28a89ff4e57803c4a7
Amila Rangana <amilarangana@Amilas-MacBook-Pro.local>
2024-04-11 10:30:11 +0200

    Removed device id

--- lib/applications/models/application.dart
+++ lib/applications/models/application.dart
@@ -1,18 +1,18 @@
 
 class Application{
   late String? id;
-  final String deviceId;
+  // final String deviceId;
   final String applicantId;
   final String name;
   final String fbProfile;
   final int status;
 
-  Application({this.id, required this.deviceId, required this.applicantId, required this.name, required this.fbProfile, required this.status});
+  Application({this.id, required this.applicantId, required this.name, required this.fbProfile, required this.status});
 
 factory Application.fromJson(String id, Map<String, dynamic> doc) {
     return Application(
       id: id, 
-      deviceId: doc['device_id'] as String, 
+      // deviceId: doc['device_id'] as String, 
       applicantId: doc['user_id'] as String,
       name: doc['name'] as String, 
       fbProfile: doc['fb_profile'] as String,
@@ -24,7 +24,7 @@ factory Application.fromJson(String id, Map<String, dynamic> doc) {
     return {
       'user_id' :applicantId,
       'name' : name,
-      'device_id' : deviceId,
+      // 'device_id' : deviceId,
       'fb_profile' : fbProfile
     };
   }
